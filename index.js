42bf4aad8b10e1c92024b2c8b27f6600c7615ae8
marioamandio <30850605+marioamandio@users.noreply.github.com>
2018-11-19 22:19:19 +0000

    chaning the server

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
 
+app.
 app.use(bodyParser.json())
 let port = process.env.dev || 3001
 
 app.get('/api/getResults', (req, res) => {
     res.send('this is the call to server')
 })
+
+if(process.env.NODE_ENV = "production") {
+
+    app.use(express.static('client/dist'))
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    });
+}
 
 
 
